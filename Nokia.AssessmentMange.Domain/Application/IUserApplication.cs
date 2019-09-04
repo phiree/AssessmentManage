d6495b535f38a14c3885298fb53ed3dbfbdf2805
chenqiang <15810303811@139.com>
2019-09-04 20:18:48 +0800

    补交

---
+++ Nokia.AssessmentMange.Domain/Application/IUserApplication.cs
@@ -0,0 +1,18 @@
+﻿using Nokia.AssessmentMange.Domain.Application.Dtos;
+using Nokia.AssessmentMange.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nokia.AssessmentMange.Domain.Application
+{
+    public interface IUserApplication : IApplicationBase<User>
+    {
+        IEnumerable<User> GetUsers(IEnumerable<string> persionIDs);
+
+
+        User GetUser(string loginName, string passWord);
+        User GetUserByPersonID(string personID);
+        UserSearchVO GetUsers(string name, string loginName, int pageSize, int pageCurrent);
+    }
+}

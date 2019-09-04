c290e4bd5b431c5c6200ce71f845bffb984b026b
chenqiang <15810303811@139.com>
2019-09-04 20:23:00 +0800

    补交

---
+++ Nokia.AssessmentMange.Domain/Application/Dtos/PersonSearchVO.cs
@@ -0,0 +1,17 @@
+﻿using Nokia.AssessmentMange.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nokia.AssessmentMange.Domain.Application.Dtos
+{
+    public class PersonSearchVO
+    {
+        public int PageSize { get; set; }
+        public int PageCurrent { get; set; }
+        public int RowCount { get; set; }
+
+        public List<Person> PersonList { get; set; }
+
+    }
+}
---
+++ Nokia.AssessmentMange.Domain/Application/Dtos/UserSearchVO.cs
@@ -0,0 +1,16 @@
+﻿using Nokia.AssessmentMange.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nokia.AssessmentMange.Domain.Application.Dtos
+{
+    public class UserSearchVO
+    {
+        public int PageSize { get; set; }
+        public int PageCurrent { get; set; }
+        public int RowCount { get; set; }
+
+        public List<User> UserList { get; set; }
+    }
+}

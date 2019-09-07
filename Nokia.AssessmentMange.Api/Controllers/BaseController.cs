fe5a25db1ba0f9477a09adb885edf554c649eb85
chenqiang <15810303811@139.com>
2019-09-07 16:47:46 +0800

    补交

---
+++ Nokia.AssessmentMange.Api/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nokia.AssessmentMange.Api.Controllers.Authentication;
+using Nokia.AssessmentMange.Domain.DomainModels;
+
+namespace Nokia.AssessmentMange.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+
+        private IAuthenticateService _authenticateService;
+
+        public BaseController(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+
+
+        internal User GetUserInfo()
+        {
+            if (Request.Headers.ContainsKey("Authorization"))
+            {
+                return _authenticateService.ParseToken(Request.Headers["Authorization"].ToString());
+            }
+            return null;
+        }
+    }
+}
---
+++ Nokia.AssessmentMange.Api/Models/PersonAssessmentGradeVO.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Nokia.AssessmentMange.Domain.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nokia.AssessmentMange.Api.Models
+{
+    public class PersonAssessmentGradeVO
+    {
+        /// <summary>
+        /// 人员考核成绩Id
+        /// </summary>
+        [Required]
+        [JsonProperty("personAssessmentId")]
+        public string PersonAssessmentId { get; set; }
+        /// <summary>
+        /// 是否缺考
+        /// </summary>
+        [JsonProperty("isAbsent")]
+        public bool IsAbsent { get; set; }
+        /// <summary>
+        /// 是否补考
+        /// </summary>
+        [JsonProperty("isMakeup")]
+        public bool IsMakeup { get; set; }
+        /// <summary>
+        /// 各科目的成绩
+        /// </summary>
+        [JsonProperty("subjectGrades")]
+        public IList<SubjectGradeModel> SubjectGrades { get; set; }
+    }
+}

e0e19ec5a3acfc03513cf304ee30d8144dce8bd1
chenqiang <15810303811@139.com>
2019-09-09 20:04:33 +0800

    补充提交

---
+++ Nokia.AssessmentMange.Domain/Common/ResultModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nokia.AssessmentMange.Domain.Common
+{
+    public class ResultModel<T>
+    {
+        public ResultCode Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+
+    public enum ResultCode
+    {
+        success = 200,
+        error = 400
+    }
+}
